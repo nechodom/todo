c11e75ddce8ddd7fbfd582c05048a182acd69e22
Matěj Kevin Nechodom <114414249+nechodom@users.noreply.github.com>
2024-04-05 01:33:57 +0200

    idk je to mrdka

--- js/app.js
+++ js/app.js
@@ -1,12 +1,56 @@
-const inputField = document.getElementById("inputText");
-const submitButton = document.getElementById("add");
-const output = document.getElementById("text");
+let form = document.getElementById("form");
+let inputText = document.querySelector("inputText");
+let add = document.querySelector("submit");
 
-submitButton.addEventListener("click", function () {
-  const inputValue = inputField.value;
+let checkIfFoundElements = () => {
+  let inputText = document.querySelector("inputText");
+  if (inputText) {
+    console.log("Našel jsem element inputText!");
+  }
+};
 
-  const newElement = document.createElement("h2");
-  newElement.textContent = inputValue;
-
-  output.appendChild(newElement);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
 });
+
+let formValidation = () => {
+  if (textInput.value === "") {
+    console.log("error!");
+    msg.innerHTML = "Text nemůže být prázdný!";
+  } else {
+    console.log("Successly added new task!");
+    msg.innerHTML = "";
+    acceptData();
+    add.click();
+  }
+};
+
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log("data");
+  createTask();
+};
+
+let createTask = (e) => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+        <div class="todo-card">
+        <h2 id="text">${x.text}</h2>
+        <button>Upravit</button>
+        <button>Smazat</button>
+      </div>
+        `);
+  });
+  resetForm();
+};
+
+let resetForm = () => {
+  textInput.value = "";
+};
