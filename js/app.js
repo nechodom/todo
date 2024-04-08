5e9f08573981c4354c04b1fa21f3e07ad4e18a53
Matěj Kevin Nechodom <114414249+nechodom@users.noreply.github.com>
2024-04-09 00:51:41 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /**
  * TODO:
- * - [ ] dopsat create task
+ * - [x] dopsat create task
+ * - [ ] mít nový task vždy nahoře a dole ten nejstarší
  * - [ ] ziskavat data z locale storage a vlozit do data[]
  * - [ ] udelat popup s informaci o vytvoreni/chybach tasku
  */
@@ -8,6 +9,7 @@
 const form = document.querySelector("form");
 const inputText = document.querySelector("#inputText");
 const add = document.querySelector("#submit");
+const todos = document.querySelector("#todos");
 const data = [];
 
 form.addEventListener("submit", (e) => {
@@ -16,36 +18,52 @@ form.addEventListener("submit", (e) => {
 });
 
 function addTodo() {
-	// declare vars
+  // validation
+  if (inputText.value === "") {
+    console.error("error!");
+    return;
+  } else {
+    console.log("Úspěšně přidán nový task!");
+  }
 
-	// validation
-	if (inputText.value === "") {
-		console.error("error!");
-		return;
-	} else {
-		console.log("Úspěšně přidán nový task!");
-	}
-
-	// process data
+  // process data
   data.push({
     text: inputText.value,
   });
   localStorage.setItem("data", JSON.stringify(data));
 
-	// create task
-  const template = `
+  // create task
+  let len = data.length - 1;
+  todos.innerHTML += `
     <div class="todo-card">
-      <h2 id="text">${data[0].text}</h2>
+      <h2 id="text">${data[len].text}</h2>
       <button>Upravit</button>
       <button>Smazat</button>
     </div>
   `;
 
-	// check
-	console.log("data", data)
-	console.log("template", template)
+  // check
+  console.log("data", data);
 
-	// reset form
+  // reset form
   inputText.value = "";
 }
 
+function printTodo() {
+  let localData = localStorage.getItem("data");
+  const obj = JSON.parse(localStorage.getItem("data"));
+  if (localData) {
+    for (let i = 0; i < obj.length; i++) {
+      todos.innerHTML += `
+        <div class="todo-card">
+          <h2 id="text">${obj[i].text}</h2>
+          <button>Upravit</button>
+          <button>Smazat</button>
+        </div>
+      `;
+    }
+  } else {
+    return;
+  }
+}
+printTodo();
